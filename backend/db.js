b28f9c80871b3af0616d71b6c17e491400811eec
Ghezlan Lameche <ghezlan@student.dz>
2025-12-28 13:00:31 +0100

    Fix PostgreSQL connection strings

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg'); // PostgreSQL
 
 const pool = new Pool({
-    host: dpg-d58gbd6mcj7s73cggb0g-a,
+    host: 'dpg-d58gbd6mcj7s73cggb0g-a',      
     port: 5432,
-    user: yassmin_db_user,
-    password: MZo6Nxf0C8FZ89EyqUEG7pqeDpVSkSta,
-    database: yassmin_db,
+    user: 'yassmin_db_user',                  
+    password: 'MZo6Nxf0C8FZ89EyqUEG7pqeDpVSkSta', 
+    database: 'yassmin_db',                    
     ssl: { rejectUnauthorized: false } 
 });
 
